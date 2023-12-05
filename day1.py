742927c5801565ebcaf416c8c2c989c22981fd43
shauryachawla <emorres25@gmail.com>
2023-12-05 12:43:52 +0530

    day 1 part 2 correct answer

--- day1.py
+++ day1.py
@@ -1,40 +1,15 @@
-import re
-      
-def replace_words_with_numbers(input_string):
-    # Define a dictionary mapping words to their numerical representations
-    word_to_number = {
-        'one': '1',
-        'two': '2',
-        'three': '3',
-        'four': '4',
-        'five': '5',
-        'six': '6',
-        'seven': '7',
-        'eight': '8',
-        'nine': '9'
-    }
+f = open("./inputs/input1-1.txt")
 
-    # Define a regular expression pattern to find the words
-    pattern = re.compile('|'.join(re.escape(word) for word in word_to_number.keys()), re.IGNORECASE)
+sum = 0
 
-    # Use the sub method to replace matched words with their numerical representations
-    result_string = pattern.sub(lambda x: word_to_number[x.group(0).lower()], input_string)
-
-    return result_string
-
-def first_and_last_numbers(line):
-    digits = [x for x in line if x.isdigit()]
-    return (digits[0], digits[-1])
-
-def calculate_answer(f):
-    sum = 0
-    for line in f:
-        line = replace_words_with_numbers(line)
-        numbers = first_and_last_numbers(line)
-        sum += int(numbers[0] + "" + numbers[1])
-    return sum
-
-if __name__ == "__main__": 
-    f = open("./inputs/input1-1.txt", "r")
-    answer = calculate_answer(f)
-    print(answer)
+for line in f:
+    digits = []
+    for i, c in enumerate(line):
+        if(c.isdigit()): 
+            digits.append(int(c))
+        for j, val in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+            if(line[i:].startswith(val)): 
+                digits.append(j+1)
+    sum += int(str(digits[0]) + "" + str(digits[-1]))
+    
+print(sum)
